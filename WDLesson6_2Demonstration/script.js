987b67c7b6915f74b605b06ba1789778581b017f
Renne Castro <rcastro2@users.noreply.github.com>
2026-03-20 11:27:35 +0000

    adds Lesson 6.2 Demonstration

---
+++ WDLesson6_2Demonstration/script.js
@@ -0,0 +1,58 @@
+/*
+//In a previous lesson we store related information in separate parallel.  Not a good approach.
+let meat_titles = ["Beef Tips","Brisket","Ribeye","Steak"]
+let meats = ["beeftips.jpg","brisket.jpg","ribeye.jpg","steak.jpg"];
+let meat_prices = [18.99,27.99,21.99,15.99];
+*/
+function example1(){
+  let meat1 = {
+    "name":"Beef Tips",
+    "image":"beeftips.jpg",
+    "price":18.99,
+  }
+  console.log(meat1.name);
+  console.log(meat1.image);
+  console.log(meat1.price);
+
+}
+function example2(){
+  let meat2 = {
+    "name":"Brisket",
+    "image":"brisket.jpg",
+    "price":27.99,
+  }
+  let meat3 = {
+    "name":"Ribeye",
+    "image":"ribeye.jpg",
+    "price":21.99,
+  }
+  let meat4 = {
+    "name":"Steak",
+    "image":"steak.jpg",
+    "price":15.99,
+  }
+  let output = document.getElementById("output");
+  //Backtick allows us to specify a string across multiple lines alleviating the need for multiple “build+=”
+  let build = `
+  <div class="card">;
+      <h3>${meat2.name}</h3>
+      <img src="images/${meat2.image}">
+      <p>${meat2.price}</p>
+  </div>`;
+
+  build += `
+  <div class="card">;
+      <h3>${meat3.name}</h3>
+      <img src="images/${meat3.image}">
+      <p>${meat3.price}</p>
+  </div>`;
+
+  build += `
+  <div class="card">;
+      <h3>${meat4.name}</h3>
+      <img src="images/${meat4.image}">
+      <p>${meat4.price}</p>
+  </div>`;
+
+  output.innerHTML = build;
+}
