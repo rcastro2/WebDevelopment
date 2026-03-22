2de56178e3688cb86da51a4ef856454320787c26
Renne Castro <rcastro2@users.noreply.github.com>
2026-03-22 12:00:52 +0000

    add Lesson 6.2 Practical

---
+++ WDLesson6_2Practical/script.js
@@ -0,0 +1,24 @@
+function BHS(){
+  let school = {
+    "name":"Bayside High School",
+    "image":"https://imagescdn.homes.com/i2/DR3rh3ZAFGmxdce_vrcZB-VfI1qR7cPIq11ixKmzhCY/117/bayside-high-school-bayside-ny-2-schoolphoto.jpg",
+    "address": "32-24 Corporal Kennedy Street, Bayside NY 11361",
+  }
+  let output = document.getElementById("output");
+  //Challenge 1: Create and display a card of the information contained in the JSON variable school
+
+}
+function artist(){
+  //Challenge 2: Fill the JSON below with the specified information
+  let artist = {
+    "name":"",
+    "image":"",
+    "album":"",
+    "url":""
+  }
+  let output = document.getElementById("output");
+  //Challenge 3: Build a card for the information in the JSON.  Make the image a hyperlink to the url provided.
+
+}
+
+
