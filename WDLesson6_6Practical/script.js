abaf5ca3b20e59f3aa54f670dc88dec9cd4449d8
Renne Castro <rcastro2@users.noreply.github.com>
2026-04-08 11:28:17 +0000

    updates Lesson 6.6 Practical

--- WDLesson6_6Practical/script.js
+++ WDLesson6_6Practical/script.js
@@ -13,9 +13,14 @@ async function init(){
     let complaint = data[i];
     build += `<div class="fitted card">
                  <h3>${complaint.complaint_type}</h3>
+                 <hr>
                  <p>${complaint.borough}</p>
                  <p>${complaint.incident_zip}</p>
                  <p>${complaint.descriptor}</p>
+                 <hr>
+                 <p>${complaint.created_date}</p>
+                 <hr>
+                 <p>${complaint.agency}</p>
             </div>`    
   }
   output.innerHTML = build;
@@ -34,9 +39,14 @@ function filterByBorough(){
     if(complaint.borough == borough){
       build += `<div class="fitted card">
                  <h3>${complaint.complaint_type}</h3>
+                 <hr>
                  <p>${complaint.borough}</p>
                  <p>${complaint.incident_zip}</p>
                  <p>${complaint.descriptor}</p>
+                 <hr>
+                 <p>${complaint.created_date}</p>
+                 <hr>
+                 <p>${complaint.agency}</p>
             </div>`;
       ct += 1;
     }
