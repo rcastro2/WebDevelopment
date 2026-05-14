ee95c08452a6481be1e22ac760827987d6f366f4
Renne Castro <rcastro2@users.noreply.github.com>
2026-05-14 13:25:31 +0000

    adds Lesson 7.4 Demonstration Template

---
+++ WDLesson7_4TemplateStarter/helper.js
@@ -0,0 +1,38 @@
+// get() returns the element using document.getElementById().
+function get(id){
+  return document.getElementById(id);
+}
+
+// showMap() displays the map for a location [lat, lon] in the right panel
+function showMap(lat, lon){
+  //alert(location);
+  let location = [lat, lon];
+  //Line below needed to create the map object once.
+  if(!mapObj){
+      mapObj = L.map("map");
+  } 
+  let map = mapObj.setView(location, 14);// [lat, lon], zoom
+
+  const tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+  }).addTo(map);
+
+  let marker = L.marker(location).addTo(map);// places marker on map
+}
+
+// card() accepts JSON of one complaint and returns a card for the complaint
+function card( info ){ 
+  let build = `<div class="card fitted">
+              <h3>${info.complaint_type}</h3>
+              <h5>${info.borough}</h5>
+              <p>${info.incident_zip}</p>
+              <p>${info.descriptor}</p><hr>
+              <p>${info.agency_name}<br>(${info.status})</p>
+        </div>`;
+  return build;
+}
+
+
+
+
