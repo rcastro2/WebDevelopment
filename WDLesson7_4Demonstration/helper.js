4d422b553026ce1c7d0454f1d90e2e0260d571e5
Renne Castro <rcastro2@users.noreply.github.com>
2026-05-15 14:15:41 +0000

    updates Lesson 7.4 Demonstration

--- WDLesson7_4Demonstration/helper.js
+++ WDLesson7_4Demonstration/helper.js
@@ -25,14 +25,21 @@ function showMap(lat, lon){
 function card( info ){ 
 
   let location = [info.latitude, info.longitude];
+  //Some complaints may not have latitudes and longitudes
+  //only create a button if these values exists
+  let mapButton = "";
+  if(info.latitude && info.longitude){
+    mapButton = `<input type="button" onclick="showMap( ${location} )" value="Map">`
+  }
+
   let build = `<div class="card fitted">
-              <h3>${info.complaint_type}</h3>
-              <h5>${info.borough}</h5>
-              <p>${info.incident_zip}</p>
-              <p>${info.descriptor}</p><hr>
-              <p>${info.agency_name}<br>(${info.status})</p>
-              <input type="button" onclick="showMap( ${location} )" value="Map">
-        </div>`;
+                  <h3>${info.complaint_type}</h3>
+                  <h5>${info.borough}</h5>
+                  <p>${info.incident_zip}</p>
+                  <p>${info.descriptor}</p><hr>
+                  <p>${info.agency_name}<br>(${info.status})</p>
+                  ${mapButton}
+              </div>`;
   return build;
 }
 
